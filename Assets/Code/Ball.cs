0755824b5ea17f52c0db69050ff63042072293e4
Joe <wirelesstaco@gmail.com>
2014-04-27 00:47:40 -0400

    blue ball, 2 player angle, Transparent indicator

--- Assets/Code/Ball.cs
+++ Assets/Code/Ball.cs
@@ -4,14 +4,28 @@ using System.Collections;
 public class Ball : MonoBehaviour {
 	public float Power = 200f;
 	public float Velicity = 3f;
+	public Material Player1Mat;
+	public Material Player2Mat;
+
+	private int playerNumber = 0;
 	// Use this for initialization
-	public void Move (Vector3 direction, float ballSize) {
+	public void Move (Vector3 direction, float ballSize,int playerNum) {
+		playerNumber = playerNum;
 		float fwdVelocity = Velicity - (ballSize/6);
-		Vector3 forceDirection = new Vector3(direction.x, direction.y, fwdVelocity) * Power;
+		Vector3 forceDirection = new Vector3(direction.x, direction.y, fwdVelocity *direction.z) * Power;
 		rigidbody.AddForce(forceDirection);
+		setMaterial();
+	}
+
+	private void setMaterial() 
+	{
+		if(playerNumber == 1){
+			renderer.material = Player1Mat;
+		}else if(playerNumber == 2){
+			renderer.material = Player2Mat;
+		}
 	}
 	private void RemoveMe(){
-		Debug.Log ("X " + transform.position.x + " Y " + transform.position.y + " Z" + transform.position.z);
 
 		if (transform.position.x > 20 || transform.position.x < -20 || transform.position.y > 20 || transform.position.y < -10 || transform.position.z > 30 || transform.position.z < -30) {
 
@@ -22,5 +36,11 @@ public class Ball : MonoBehaviour {
 	private void Update()
 	{
 		RemoveMe ();
+	}
+
+	private void OnCollisionEnter(){
+		Debug.Log ("hit");
+		//disable physic collisions
+		//collider.isTrigger = true;
 	}
 }
---
+++ Assets/Code/Player.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public GameObject Ball;
+	public GameObject Indicator;
+	public int Speed = 5;
+	public float StartSize = 1f;
+	public float GrowSpeed = 2f;
+	public int PlayerNum = 1;
+
+	private float ballSize = 1f;
+	private Vector3 startPos;
+	private Vector3 endPos;
+	private GameObject gameIndicator;
+
+	private string shoot;
+
+	void Start ()
+	{
+		gameIndicator = (GameObject)Instantiate (Indicator, transform.position, transform.rotation) as GameObject;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Control Spawner
+		float xMove = 0;
+		float yMove = 0;
+		int playerSign;
+		if (PlayerNum == 1) {
+			xMove = Input.GetAxis ("Horizontal");
+			yMove = Input.GetAxis ("Vertical");
+			shoot = "Fire1";
+			playerSign = 1;
+
+		} else{
+			xMove = Input.GetAxis ("HorizontalP2");
+			yMove = Input.GetAxis ("VerticalP2");
+			shoot = "Fire2";
+			playerSign = -1;
+
+		}
+		Vector3 movement = new Vector3 (xMove * Speed, yMove * Speed, 0);
+		transform.Translate	(movement * Time.deltaTime);
+		UpdateIndicator ();
+
+		if (Input.GetButtonDown (shoot)) {
+			//Set Start
+			startPos = transform.localPosition;
+			gameIndicator.renderer.enabled = true;
+
+		}
+
+		if(Input.GetButtonUp (shoot))
+		{
+			gameIndicator.renderer.enabled = false;
+			//Shoot angle
+			endPos = transform.localPosition;
+
+	
+			Vector3 forceDirection = endPos - startPos;
+
+			//Set Z
+			forceDirection.z = playerSign;
+			Debug.Log (endPos.x);
+			Ball.transform.rotation = Quaternion.LookRotation(endPos);
+
+
+			//Ball scale
+			Ball.transform.localScale = new Vector3( ballSize , ballSize , ballSize );
+			GameObject instance = (GameObject)Instantiate (Ball, transform.position, transform.rotation) as GameObject;
+			instance.GetComponent<Ball>().Move(forceDirection , ballSize ,PlayerNum);
+
+			//Set Mass
+			instance.GetComponent<Rigidbody>().mass = ballSize;
+
+			//Rest ball size
+			ballSize = StartSize;
+
+		}
+
+		if (Input.GetButton (shoot)) 
+		{	
+			ballSize += GrowSpeed * Time.deltaTime;
+
+			UpdateIndicator();
+		}
+	}
+
+	void UpdateIndicator () 
+	{
+		gameIndicator.transform.localScale = new Vector3 (ballSize, ballSize, ballSize);
+		gameIndicator.transform.position = transform.position;
+
+	}
+}
